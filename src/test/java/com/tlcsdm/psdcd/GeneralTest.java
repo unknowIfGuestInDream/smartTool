521fd76ea96a2978c4e9447be9bfecc42aae1248
Liang Tang <57802425+UnknownInTheDream@users.noreply.github.com>
2022-09-19 17:10:59 +0800

    Update GeneralTest.java

--- src/test/java/com/tlcsdm/psdcd/GeneralTest.java
+++ src/test/java/com/tlcsdm/psdcd/GeneralTest.java
@@ -11,12 +11,13 @@ import org.junit.Before;
 import org.junit.Test;
 
 import com.tlcsdm.common.BaseUtils;
+import com.tlcsdm.diff.DiffHandleUtils;
 
 import cn.hutool.core.io.FileUtil;
 import cn.hutool.core.lang.Console;
 import cn.hutool.core.lang.ConsoleTable;
-import cn.hutool.core.util.CharsetUtil;
 import cn.hutool.core.util.StrUtil;
+import cn.hutool.poi.excel.BigExcelWriter;
 import cn.hutool.poi.excel.ExcelReader;
 import cn.hutool.poi.excel.ExcelUtil;
 import cn.hutool.poi.excel.cell.CellLocation;
@@ -37,7 +38,7 @@ public class GeneralTest {
 	// 忽略的sheet
 	private final List<String> ignoreSheetNames = List.of("Overview", "Summary", "Sample-CT");
 	// 如果markSheetNames不为空，那么只会读取当前对象内的sheet, 忽略ignoreSheetNames配置
-	private final List<String> markSheetNames = List.of("DMA");
+	private final List<String> markSheetNames = List.of();
 	// excel的父级目录路径
 	private final String parentDirectoryPath = "C:\\workspace\\test";
 	// excel文件名称
@@ -49,7 +50,7 @@ public class GeneralTest {
 	// excel中Generate Files值对应的Cell
 	private final String generateFileCell = "C15";
 	// Generate Files的父级路径, 如果为空，则默认为parentDirectoryPath值
-	private final String generateFilesParentPath = "C:\\workspace\\test";
+	private final String generateFilesParentPath = "C:\\runtime-smc.rh850.product\\src\\smc_gen\\general";
 
 	/*
 	 * 待初始化对象
@@ -173,11 +174,75 @@ public class GeneralTest {
 	 */
 	@Test
 	public void generalTest2() {
-		String sheetName = "DMA";
-		String fileType = "h";
-		ExcelReader reader = ExcelUtil.getReader(FileUtil.file(parentDirectoryPath, excelName), sheetName);
-		FileUtil.writeString(reader.readAsText(false), FileUtil.file(parentDirectoryPath, sheetName + "." + fileType),
-				CharsetUtil.CHARSET_UTF_8);
+		File testFile = FileUtil.file(parentDirectoryPath, excelName);
+		Map<String, String> generateFileMap = new HashMap<>();
+		for (String sheetName : sheetNames) {
+			BigExcelWriter excelWriter = ExcelUtil.getBigWriter();
+			logHandler("========================= Begin Reading " + sheetName + " =========================", 1);
+			logHandler("Start reading sheet: " + sheetName, 1);
+			ExcelReader reader = ExcelUtil.getReader(testFile, sheetName);
+			String endCell = getEndCell(reader);
+			logHandler("endCell: " + endCell, 1);
+			CellLocation start = ExcelUtil.toLocation(startCell);
+			CellLocation end = ExcelUtil.toLocation(endCell);
+			int startX = start.getX();
+			int startY = start.getY();
+			int endX = end.getX();
+			int endY = end.getY();
+			String generateFileName = reader.getCell(generateFileCell).getStringCellValue();
+			generateFileMap.put(sheetName, generateFileName);
+			excelWriter.setSheet(sheetName);
+			List<List<String>> list = new ArrayList<>(endY - startY + 1);
+			for (int j = startY; j <= endY; j++) {
+				List<String> l = new ArrayList<>(endX - startX + 1);
+				boolean isDefine = false;
+				String firstValue = BaseUtils.valueOf(CellUtil.getCellValue(reader.getCell(startX, j)));
+				if ("#define".equals(firstValue) || "#ifndef".equals(firstValue)) {
+					isDefine = true;
+				}
+				for (int j2 = startX; j2 <= endX; j2++) {
+					String cellValue = "";
+					// 首列的开头保留空格
+//					if (j2 == startX) {
+//						cellValue = StrUtil.trimEnd(BaseUtils.valueOf(CellUtil.getCellValue(reader.getCell(j2, j))));
+//					} else {
+//						cellValue = StrUtil.trim(BaseUtils.valueOf(CellUtil.getCellValue(reader.getCell(j2, j))));
+//					}
+					cellValue = BaseUtils.valueOf(CellUtil.getCellValue(reader.getCell(j2, j)));
+					if (isDefine && j2 < endX) {
+						cellValue += " ";
+					}
+					l.add(cellValue);
+				}
+				list.add(l);
+			}
+			excelWriter.write(list, false);
+			File file = FileUtil.file(parentDirectoryPath + "\\result", sheetName + ".xlsx");
+			excelWriter.flush(file);
+			excelWriter.close();
+			logHandler("========================= End Reading " + sheetName + " =========================", 1);
+		}
+
+		for (String sheetName : sheetNames) {
+			ExcelReader reader = ExcelUtil.getReader(parentDirectoryPath + "\\result\\" + sheetName + ".xlsx",
+					sheetName);
+			String generateFileName = generateFileMap.get(sheetName);
+			FileUtil.writeUtf8String(reader.readAsText(false).replaceAll("\\t", ""),
+					FileUtil.file(parentDirectoryPath + "\\result\\files", generateFileName));
+			reader.close();
+			logHandler("========================= Begin Comparing " + generateFileName + " =========================",
+					1);
+			String generateFileParent = generateFilesParentPath.length() == 0 ? parentDirectoryPath
+					: generateFilesParentPath;
+			File generateFile = FileUtil.file(generateFileParent, generateFileName);
+			if (FileUtil.exist(generateFile)) {
+				List<String> diffString = DiffHandleUtils.diffString(
+						parentDirectoryPath + "\\result\\files\\" + generateFileName,
+						generateFileParent + "\\" + generateFileName);
+				DiffHandleUtils.generateDiffHtml(diffString, parentDirectoryPath + "\\result\\" + sheetName + ".html");
+			}
+			logHandler("========================= End Comparing " + generateFileName + " =========================", 1);
+		}
 	}
 
 	/**
