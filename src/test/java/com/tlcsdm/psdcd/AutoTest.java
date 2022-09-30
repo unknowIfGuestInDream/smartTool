896f44046ec26ddd875de0240895b3193cb0a014
Liang Tang <57802425+UnknownInTheDream@users.noreply.github.com>
2022-09-30 17:10:51 +0800

    Create AutoTest.java

---
+++ src/test/java/com/tlcsdm/psdcd/AutoTest.java
@@ -0,0 +1,53 @@
+package com.tlcsdm.psdcd;
+
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.sikuli.basics.Settings;
+import org.sikuli.script.App;
+import org.sikuli.script.FindFailed;
+import org.sikuli.script.ImagePath;
+import org.sikuli.script.Region;
+import org.sikuli.script.Screen;
+
+public class AutoTest {
+
+	@BeforeClass
+	public static void init() {
+		Settings.ActionLogs = false;
+		Settings.InfoLogs = false;
+		ImagePath.setBundlePath("C:\\workspace\\test\\autotestImage");
+	}
+
+	@Test
+	public void autoTest1() throws FindFailed {
+		App app = App.open("C:\\workspace\\rcp\\V1.7\\eclipse\\SmartConfigurator.exe", 10);
+		Region screen = new Screen();
+		screen.wait("catchSmartButtons", 5);
+		screen.click("newConfigurationFile");
+
+		screen.exists("RH850U2A16", 2).click();
+		screen.click("unexpand");
+		screen.exists("RH850U2A16_516pin", 2).click();
+		screen.click("unexpand");
+		screen.exists("R7F702300EBBG", 1).click();
+		screen.type("fileName", "testapi");
+		screen.click("button_finish");
+		if (screen.has("hasSameFileName", 2000)) {
+			screen.click("button_yes");
+		}
+
+		screen.exists("tab_components", 3).click();
+		screen.exists("addComponent", 5).click();
+		if (!screen.has("window_newComponent", 3000)) {
+			throw new FindFailed("window_newComponent not found");
+		}
+		// screen.exists("component_dmaController", 5).click();
+		// screen.exists("component_riicMaster", 5).click();
+		// screen.wheel("component_dmaController", Button.WHEEL_DOWN, 10);
+//		screen.
+//		IRobot r = screen.getScreen().getRobot();
+//		r.mouseWheel(Button.WHEEL_DOWN);
+//		r.delay(50);
+		screen.dragDrop("scroll_down");
+	}
+}
